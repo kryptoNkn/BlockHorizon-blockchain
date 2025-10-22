d3f7079a5ba501aa3eded553cf14c53b38961f23
kryptoNkn <andrej.art3mov@yandex.ru>
2025-10-22 16:40:16 +0300

    Refactor balances API and add unit test
    Made Pallet::new public and updated set_balance and balance methods to take String by value for consistency. Removed unused code from main.rs and added a unit test to verify balances functionality.

--- src/balances.rs
+++ src/balances.rs
@@ -1,24 +1,23 @@
 use std::collections::BTreeMap;
-use crate::balances;
 
 pub struct Pallet {
     balances: BTreeMap<String, u128>,
 }
 
 impl Pallet {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-             balances:  BTreeMap::new(),
+            balances: BTreeMap::new(),
         }
     }
 
     // set the balance of an account `who` to some `amount`
-    pub fn set_balance(&mut self, who: &String, amount: u128) {
-        self.balances.insert(who.clone(), amount);
+    pub fn set_balance(&mut self, who: String, amount: u128) {
+        self.balances.insert(who, amount);
     }
 
     // get the balance of an account `who`
-    pub fn balance(&self, who: &String) -> u128 {
-        *self.balances.get(who).unwrap_or(&0)
+    pub fn balance(&self, who: String) -> u128 {
+        *self.balances.get(&who).unwrap_or(&0)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 mod balances;
-use std::collections::BTreeMap;
-
 
 fn main() {
     println!("Hello, world!");
-    let mut map = BTreeMap::new();
-    map.insert("alice", 100);
-    assert_eq!(map.get(&"alice").unwrap_or(&0), &100);
-    assert_eq!(map.get(&"bob"), None);
+}
 
-    let maybe_value = map.get(&"bob");
-    match maybe_value {
-        Some(value) => {
-            println!("{value}")
-        },
-        None => {
-            println!("no value")
-        },
-    }
+#[test]
+fn init_balances() {
+    let mut balances = balances::Pallet::new();
+
+    assert_eq!(balances.balance("alice".to_string()),0);
+    balances.set_balance("alice".to_string(), 100);
+    assert_eq!(balances.balance("alice".to_string()), 100);
+    assert_eq!(balances.balance("bob".to_string()), 0);
 }
