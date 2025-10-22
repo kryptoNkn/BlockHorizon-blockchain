7e6eaabe7092550b511b1452e038509511e9e3e2
kryptoNkn <andrej.art3mov@yandex.ru>
2025-10-22 22:06:45 +0300

    Refactor blockchain structure and remove balances module
    Removed the balances module and related code from main.rs. Added new block.rs and lib.rs files to define Block struct and utility functions for blockchain operations. Updated dependencies to include the hex crate for block hash encoding.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-mod balances;
-use std::collections::BTreeMap;
-
 fn main() {
-    println!("Hello, world!");
-    let mut pallet = balances::Pallet::new();
-
-    let number: u64 = 1;
-    let number2 = number.checked_sub(Some(10).unwrap());
-    println!("number2 = {}", number2.unwrap_or(0));
+    
 }
  
