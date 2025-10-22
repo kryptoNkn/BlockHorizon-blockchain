f584d47d25b2eff06f2c7db6dde3c413533abe0e
kryptoNkn <andrej.art3mov@yandex.ru>
2025-10-22 18:12:24 +0300

    Add transfer function to Pallet struct
    Introduces a transfer method to the Pallet struct, allowing balance transfers between accounts with checks for sufficient funds and overflow. Also refactors code formatting for improved readability.

--- src/balances.rs
+++ src/balances.rs
@@ -6,9 +6,7 @@ pub struct Pallet {
 
 impl Pallet {
     pub fn new() -> Self {
-        Self {
-            balances: BTreeMap::new(),
-        }
+        Self { balances: BTreeMap::new() }
     }
 
     // set the balance of an account `who` to some `amount`
@@ -20,17 +18,30 @@ impl Pallet {
     pub fn balance(&self, who: String) -> u128 {
         *self.balances.get(&who).unwrap_or(&0)
     }
-}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn init_balances() {
-    let mut balances = super::Pallet::new();
+    // transfer `amount` from one account to another
+    // this function verifies that `from` has at least `amount` balance to transfer
+    // and that no mathematical overflows occur
+    pub fn transfer(
+        &mut self, 
+        caller: String, 
+        to: String, 
+        amount: u128
+    ) -> Result<(), &'static str> {
+        let caller_balance = self.balance(&caller);
+        let to_balance = self.balance(&to);
 
-    assert_eq!(balances.balance("alice".to_string()),0);
-    balances.set_balance("alice".to_string(), 100);
-    assert_eq!(balances.balance("alice".to_string()), 100);
-    assert_eq!(balances.balance("bob".to_string()), 0);
+        let new_caller_balance = caller_balance
+        .checked_sub(amount)
+        .ok_or("Insufficient balance")?;
+
+        let new_to_balance = to_balance
+        .checked_sub(amount)
+        .ok_or("Overflow when adding to balance")?;
+
+        self.set_balance(&caller, new_caller_balance);
+        self.set_balance(&to, new_to_balance);
+
+        Ok(())
     }
 }
