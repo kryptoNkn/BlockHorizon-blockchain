92ad209c07dbe62e20a3a79bd66f33f8570c3fd6
kryptoNkn <andrej.art3mov@yandex.ru>
2025-10-24 17:14:41 +0300

    Add difficulty to Block and implement mining logic
    Introduces a `difficulty` field to the Block struct and updates the constructor accordingly. Adds a `mine` method to perform proof-of-work mining based on the difficulty, and implements a `check_difficulty` function to validate hashes. Updates the Hashable implementation to include the new difficulty field.

--- src/block.rs
+++ src/block.rs
@@ -8,6 +8,7 @@ pub struct Block {
     pub prev_block_hash: BlockHash,
     pub nonce: u64,
     pub payload: String, // don't kill me pls <3 etc
+    pub difficulty: u128,
 }
 
 impl Debug for Block {
@@ -23,7 +24,7 @@ impl Debug for Block {
 
 impl Block {
     pub fn new(index: u32, timestamp: u128, prev_block_hash: BlockHash,
-    nonce: u64, payload: String) -> Self {
+    nonce: u64, payload: String, difficulty: u128) -> Self {
         Block {
             index,
             timestamp,
@@ -31,6 +32,20 @@ impl Block {
             prev_block_hash,
             nonce,
             payload,
+            difficulty,
+        }
+    }
+
+    // mining implementation
+    pub fn mine(&mut self) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+
+            if check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
         }
     }
 }
@@ -43,7 +58,12 @@ impl Hashable for Block {
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
         bytes.extend(self.payload.as_bytes());
+        bytes.extend(&u128_bytes(&self.difficulty));
 
         bytes
     }
+}
+
+pub fn check_difficulty(hash: &BlockHash, difficulty: u128) -> bool {
+    difficulty > difficulty_bytes_as_u128(&hash)
 }
