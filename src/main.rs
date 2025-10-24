fd4b27452431747989aec9abea6e771cddb6c032
kryptoNkn <andrej.art3mov@yandex.ru>
2025-10-24 17:15:58 +0300

    Remove block creation and hashing from main
    Deleted example code in main that created and hashed a block, leaving the main function empty. This cleans up the entry point for future development.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 use BlockHorizonLib::*;
 
 fn main() {
-    let mut block = Block::new(0,0,vec![0;32],0,"Genesis block!".to_owned());
-    println!("{:?}", &block);
-
-    let h = block.hash();
-    println!("{:?}", h);
-
-    block.hash = h;
-    println!("{:?}", &block);
+    
 }
