43bba00d2bba013a9db5882845cf5c7fc20f83b7
kryptoNkn <andrej.art3mov@yandex.ru>
2025-10-28 20:46:50 +0300

    Add genesis block validation to blockchain
    Added a check to ensure the genesis block's prev_block_hash is valid. Also improved error reporting for difficulty check failures.

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -11,7 +11,16 @@ impl BlockChain {
                 println!("Index mismatch {} != {}", &block.index, &i);
                 return false;
             } else if !block::check_difficulty(&block.hash, block.difficulty) {
-
+                println!("Difficulty fail");
+                return false;
+            } else if i != 0 {
+                // Not genesis block
+            } else {
+                // Genesis block
+                if block.prev_block_hash != vec![0; 32] {
+                    println!("Genesis block prev_block_hash invalid");
+                    return false;
+                }
             }
         }
         true
